d665593bf65b888805cb66410b08bdde5d01aaab
Lewis Ritz <lewisritz@lewisritz.com>
2014-01-18 12:12:33 -0500

    changed index.html

--- fileServer.js
+++ fileServer.js
@@ -8,4 +8,4 @@ app.listen(process.env.PORT || 8080);
 
 console.log("App listening on port 8080");
 var d = new Date();
-console.log("Date: "+d.getFullYear()+"-"+d.getMonth());
+console.log("Date: "+d.getFullYear()+"-"+(d.getMonth()+1)+"-"+d.getDay()+" "+d.get);
